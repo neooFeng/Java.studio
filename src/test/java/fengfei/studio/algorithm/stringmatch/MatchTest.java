4e1e9e9668f7774f54c3dba74457f7769e6c0435
Feng Fei <china.arq@qq.com>
2019-08-05 17:37:54 +0800

    fix

--- src/test/java/fengfei/studio/algorithm/stringmatch/MatchTest.java
+++ src/test/java/fengfei/studio/algorithm/stringmatch/MatchTest.java
@@ -2,13 +2,15 @@ package fengfei.studio.algorithm.stringmatch;
 
 import org.junit.Test;
 
+import java.lang.management.BufferPoolMXBean;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MatchTest {
     final int loopCount = 100;
     final int textLength = 1000000;
-    final String target = "驼绍经贯奏春帮珍玻毒型挂封持项垮挎城挠政赴赵挡挺括拴拾挑指";
+    final String target = "厂七卜人入八九几儿了力乃刀又三于干亏士工土才寸下大丈与万上小口巾山千乞川亿个勺久凡及夕丸么广亡门义之尸弓己已子卫也女飞刃习叉马乡丰王井开夫天无元专云扎艺木五支厅不太犬区历尤友匹车巨牙屯比互切瓦止少日中冈贝内水见午牛手毛气升长仁什片仆化仇币仍仅斤爪反介父从今凶分乏公仓月氏勿欠风丹匀乌凤勾文";
 
     @Test
     public void testBFPerformance(){
