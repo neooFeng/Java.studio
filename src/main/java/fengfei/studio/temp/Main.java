0bfba6e5cae7a1f7f80bf4f5a1ebd862cbca7388
fengfei <fengfei@feifanuniv.com>
2020-10-20 09:04:36 +0800

    add

--- src/main/java/fengfei/studio/temp/Main.java
+++ src/main/java/fengfei/studio/temp/Main.java
@@ -2,6 +2,26 @@ package fengfei.studio.temp;
 
 public class Main {
     public static void main(String[] args) {
+        Integer integer = null;
 
+        int a = 0;
+        if (a = 123){
+            System.out.println("ok");
+        }
+
+        if (null == integer){
+            System.out.println("ok2");
+        }
+
+
+        if (1 == integer){
+            System.out.println("ok");
+        }else{
+            System.out.println("ok");
+        }
+
+        if (integer != 1){
+            System.out.println("ok2");
+        }
     }
 }
